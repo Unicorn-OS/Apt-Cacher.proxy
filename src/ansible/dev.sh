4af3ded8a7202ab6473fd90c758121e7ad77b40a
UniSocial <uni@ultura.net>
2024-10-09 17:16:28 -0400

    try

--- src/ansible/dev.sh
+++ src/ansible/dev.sh
@@ -1,4 +1,7 @@
 cd roles
 
 git clone git@github.com:Unicorn-OS/ansible-role-AptCacherNg-Client.git
-ln -srf ansible-role-AptCacherNg-Client depends/UniInc.AptCacherNg-Client
+rm -r depends/UniInc.AptCacherNg-Client
+
+git clone git@github.com:Unicorn-OS/ansible-role-AptCacherNg-Server-inContainer.git
+rm -r depends/UniInc.AptCacherNg-Server-inContainer
